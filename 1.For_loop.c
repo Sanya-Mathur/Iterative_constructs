7607cf424f298914025ffae4cc21ea5b20e39c73
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-21 08:49:01 +0530

    Create 1.For_loop.c

---
+++ 1.For_loop.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010042\n")
+for(int i=0;i<10;i++)//this is called a for loop , generally used for fixed number of iterations
+{
+    printf("I love coding\n");
+}
+
+    return 0;
+}
