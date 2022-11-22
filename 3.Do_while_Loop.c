4483c800977a31bb909c7dfcfa77ab09f990a33f
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 11:23:45 +0530

    Create 3.Do_while_Loop.c

---
+++ 3.Do_while_Loop.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main ()
+{printf("RA2211042010042\n");
+ int a = 1;
+ do
+ {
+ printf("value of a: %d\n", a);
+ a++;//incrementation
+ }while( a <=10 );//condition
+ return 0;
+}
