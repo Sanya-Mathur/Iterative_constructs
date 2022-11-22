f0a78b70916774c204809a71ccb94f37c423b5a0
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 11:21:45 +0530

    Create 2.While_loop.c

---
+++ 2.While_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{printf("RA2211042010042\n");
+ int a = 1;
+ while( a <=10 )//condition
+ {
+ printf("value of a: %d\n", a);
+ a++;//incrementation
+ }
+ return 0;
+}
