943e08c5298aa089d93e966c3fbd715b4241a624
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 11:25:26 +0530

    Create 4.Break_statement.c

---
+++ 4.Break_statement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{printf("RA2211042010042\n");
+ int a = 1;
+ do
+ {
+ printf("value of a: %d\n", a);
+ a++;//incrementation
+ if(a==5)
+ break;
+ }while( a <=10 );//condition
+ return 0;
+}
