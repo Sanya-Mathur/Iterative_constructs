fd8d2aed523edfd635b07a1d58815a14f6fd926e
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 11:27:14 +0530

    Create 5.Continue_statement.c

---
+++ 5.Continue_statement.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main ()
+{printf("RA2211042010042\n");
+ int a = 1;
+ do
+ {if(a==6)
+    {
+        continue;
+    }
+ printf("value of a: %d\n", a);
+ a++;//incrementation
+ 
+ }while( a <=10 );//condition
+ return 0;
+}
